53ae6fbcc29d4c8df441bc7122d8587ef8ed7fd2
MoHammad Mohammadi <mohammadm97i@gmail.com>
2018-12-21 18:51:02 +0330

    feature: adds navbar to homepage

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, StatusBar} from 'react-native';
 import Modal from "react-native-modal";
 import Login from "./src/Login.js";
 const instructions = Platform.select({
@@ -16,7 +16,7 @@ export default class App extends Component<Props> {
     };
 
     state = {
-        loginView: true,
+        loginView: false,
         userInfo: '',
     };
 
@@ -32,6 +32,28 @@ export default class App extends Component<Props> {
     render() {
         return (
             <View style={{height: '100%', width: '100%'}}>
+                <StatusBar hidden/>
+                <View style={styles.navigationBar}/>
+
+
+                <View style={styles.navigationBase}>
+                    <View style={styles.navigationBaseItems}>
+                        <TouchableOpacity onPress={() => this.setState({loginView: true})}
+                                          style={styles.TouchableOpacityboundLeft}>
+                            <View style={styles.TouchableOpacityboundFlexLeft}>
+                                <Image style={styles.icLogout}
+                                       source={require('./src/images/icLougout/icLougout.png')}/>
+                            </View>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => this.setState({sidebarModal: true})}
+                                          style={styles.TouchableOpacityboundLeft}>
+                            <View style={styles.TouchableOpacityboundFlexLeft}>
+                                <Image style={styles.icSidebar}
+                                       source={require('./src/images/icSidebar/icSidebar.png')}/>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                </View>
 
                 <Modal
                     animationIn="slideInUp"
@@ -50,3 +72,234 @@ export default class App extends Component<Props> {
         );
     }
 }
+
+
+const styles = StyleSheet.create({
+    icBack: {
+        height: 20,
+        width: 20,
+        marginRight: 10,
+    },
+    backButtonText: {
+        fontFamily: Platform.OS === 'ios' ? "IRANYekan" : "IRANYekanRegular",
+        fontSize: 17,
+        fontWeight: Platform.OS === 'ios' ? "normal" : "normal",
+        textAlign: 'right',
+        color: '#8BEADF',
+        marginRight: 2
+    },
+    settingsModalTopBarFlex: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    settingsModalTopBarContainer: {
+        width: '100%',
+        height: 60,
+    },
+    settingsContainerModal: {
+        borderRadius: 5,
+        marginLeft: 20,
+        marginRight: 20,
+        marginTop: 40,
+        marginBottom: 40,
+        backgroundColor: '#7b7491'
+    },
+    settingsContainer: {
+        height: '100%',
+        width: '100%',
+    },
+    settingsContainerFlex: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 5,
+    },
+    webViewErrorContainer: {
+        aspectRatio: 1,
+        backgroundColor: '#7b7491',
+        borderRadius: 5
+    },
+    webViewContainer: {
+        height: '100%',
+        width: '100%'
+    },
+    sidebarTopBannerLabel: {
+        fontFamily: Platform.OS === 'ios' ? "IRANYekan" : "IRANYekanBold",
+        fontSize: 23,
+        fontWeight: Platform.OS === 'ios' ? "bold" : "normal",
+        textAlign: 'right',
+        color: '#8BEADF',
+    },
+    sidebarTopBannerImage: {
+        width: 150,
+        height: 150
+    },
+    sidebarTopBannerFlex: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    sidebarItemLabelText: {
+        fontFamily: Platform.OS === 'ios' ? "IRANYekan" : "IRANYekanRegular",
+        fontSize: 17,
+        fontWeight: Platform.OS === 'ios' ? "normal" : "normal",
+        textAlign: 'right',
+        color: '#8BEADF',
+        paddingRight: 10
+    },
+    icSidebarItem: {
+        width: 30,
+        height: 30,
+    },
+    sidebarItemFlex: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    sidebarItemContainer: {
+        maxWidth: '100%',
+        height: 55,
+        borderBottomWidth: 1,
+        borderWidth: 0,
+        borderColor: '#8BEADF',
+        borderRadius: 0,
+    },
+    sidebarTopBannerContainer: {
+        width: '100%',
+        height: 220,
+        backgroundColor: 'transparent'
+    },
+    sidebarModalStyle: {
+        width: '100%',
+        height: '100%',
+        backgroundColor: '#433E53',
+        paddingRight: 20,
+        paddingLeft: 10
+    },
+    icSidebar: {
+        width: 30,
+        height: 30
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+    navigationBar: {
+        backgroundColor: '#433E53',
+        width: '100%',
+        height: 30,
+    },
+    TouchableOpacitybound1: {
+        width: 28,
+        height: 28,
+        marginRight: 4
+    },
+    navigationBaseItems: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    TouchableOpacityboundLeft: {
+        width: 50,
+        height: 50,
+        marginLeft: 10,
+    },
+    TouchableOpacityboundFlexLeft: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    TouchableOpacityboundFlexRight: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    icWrite: {
+        width: 18,
+        height: 18,
+        backgroundColor: 'transparent',
+    },
+
+    icSettings: {
+        width: 18,
+        height: 18,
+        backgroundColor: 'transparent',
+    },
+    headerRightContainer: {
+        width: 60,
+        height: 28,
+        marginRight: 10,
+    },
+    headerRightFlex: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    navigationBase: {
+        backgroundColor: '#433E53',
+        width: '100%',
+        height: 50, //109
+        shadowColor: "rgba(67, 82, 87, 0.4)",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowRadius: 5,
+        shadowOpacity: 0,
+    },
+    navigationBaseDown: {
+        backgroundColor: 'rgb(255, 255, 255)',
+        width: '100%',
+        height: 70, //109
+        shadowColor: "rgba(67, 82, 87, 0.4)",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowRadius: 5,
+        shadowOpacity: 1,
+    },
+
+    icLogout: {
+        width: 30,
+        height: 30
+    },
+    todayButton: {
+        maxWidth: 60,
+        paddingLeft: 4,
+        paddingRight: 4,
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: 'transparent',
+        fontFamily: "IRANYekan",
+        fontSize: 17,
+        fontWeight: "normal",
+        fontStyle: "normal",
+        lineHeight: 28,
+        letterSpacing: 0,
+        textAlign: "center",
+        color: 'white',
+    },
+});
+
