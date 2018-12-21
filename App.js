8b157cb964976ac314df520e958e874c6436df0a
MoHammad Mohammadi <mohammadm97i@gmail.com>
2018-12-21 13:00:30 +0330

    feature: adds Login page to App.js

--- App.js
+++ App.js
@@ -1,41 +1,71 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-
+import Modal from "react-native-modal";
+import Login from "./src/Login.js";
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+    android:
+        'Double tap R on your keyboard to reload,\n' +
+        'Shake or press menu button for dev menu',
 });
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.loggedIn = this.loggedIn.bind(this);
+    };
+
+    state = {
+        loginView: true,
+    };
+
+    loggedIn() {
+        this.props.setModalVisible(false);
+    };
+
+    setLoggInModalVisible = visible => {
+        this.setState({loginView: visible});
+    };
+
+    render() {
+        return (
+            <View style={{height: '100%', width: '100%'}}>
+
+
+                <Modal
+                    animationIn="slideInUp"
+                    animationOut="slideOutDown"
+                    hideModalContentWhileAnimating={true}
+                    // onBackdropPress={() => this.setState({loginView: false})}
+                    isVisible={this.state.loginView}
+                    style={{margin: 0}}
+                    onRequestClose={() => {
+                        alert('Modal has been closed.');
+                    }}>
+                    <Login setLoggInModalVisible={this.setLoggInModalVisible}/>
+                </Modal>
+                {/*<Text style={styles.instructions}>{instructions}</Text>*/}
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
